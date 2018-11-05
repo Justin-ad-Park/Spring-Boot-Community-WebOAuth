9c14b6b7b76a0ed879d22c8fa14c01d0af3ebf5c
cjloveu <justin@11street.co.th>
2018-11-05 15:47:14 +0900

    application.yml : Changing the server port
    User.java : Add Comment for Principal

--- src/main/java/com/web/domain/User.java
+++ src/main/java/com/web/domain/User.java
@@ -35,7 +35,7 @@ public class User implements Serializable {
     private String email;
 
     @Column
-    private String pincipal;
+    private String pincipal;    //OAuth2 인증으로 제공받은 키값
 
     @Column
     @Enumerated(EnumType.STRING)
